e069260b7ef3a3615bc2eed91c4fa14211aaf6bc
raeraeraee <rlatjsfo674@naver.com>
2023-06-06 23:33:44 +0900

    .

--- project/app-02/src/main/java/bitcamp/myapp/App.java
+++ project/app-02/src/main/java/bitcamp/myapp/App.java
@@ -5,7 +5,7 @@ package bitcamp.myapp;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("나의 할 일 목록 관리 시스템");
+        System.out.println("나의 할 일 관리 시스템");
         System.out.println("----------------------------------");
 
         System.out.print("번호: ");
@@ -13,8 +13,6 @@ public class App {
 
         System.out.printf("이름: %s", "홍길동");
         System.out.println();
-
-        System.out.println("나이: " + 20);
 
         System.out.printf("깃 잔디: %b\n", true);
 
