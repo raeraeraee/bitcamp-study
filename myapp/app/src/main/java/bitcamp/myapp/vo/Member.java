95a6093b24cb0983ee662a0067b0ac4fb2d5720f
raeraeraee <rlatjsfo674@naver.com>
2023-06-19 17:34:46 +0900

    인터페이스를 이용하여 List 사용 규칙 정의하기

--- myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
@@ -2,13 +2,8 @@ package bitcamp.myapp.vo;
 
 public class Member {
 
-  // 모든 인스턴스가 공유하는 값은 스태틱 필드에 보관한다.
   private static int userId = 1;
 
-  // 상수는 스태틱 필드로 정의한다.
-  // 정보를 다룰 떄는 그 정보를 갖고 있는 클래스에 그 기능을 둔다.
-  // => 필드도 마찬가지다.
-  // GRASP 패턴 : Information Expert
   public static final char MALE = 'M';
   public static final char FEMALE = 'W';
 
@@ -22,20 +17,21 @@ public class Member {
     this.no = userId++;
   }
 
-  // 같은 기능을 수핸하는 생성자가 위에 있다.
+  // 같은 기능을 수행하는 생성자가 위에 있다.
   // 다만 파라미터가 다를 뿐이다.
-  // => "생성자 오버로딩(overriding)"
+  // => "생성자 오버로딩(overloading)"
   public Member(int no) {
     this.no = no;
   }
 
-  // Object의 equals()는 Member 인스턴스를 비교하는대 적합하지 않다.
-  // 왜? Object의 equals()는 단순히 인스턴스 주소가 같은지 비교하기 떄문이다.
+  // Object의 equals()는 Member 인스턴스를 비교하는데 적합하지 않다.
+  // 왜? Object의 equals()는 단순히 인스턴스 주소가 같은지 비교하기 때문이다.
   // 우리가 원하는 것은 인스턴스 주소가 다르더라도
   // 두 인스턴스 안에 저장된 변수들의 값이 같다면
   // 두 인스턴스는 같은 것으로 처리하는 것이다.
   // 그렇게 하기 위해 수퍼 클래스의 equals()를 재정의 한다.
-  // => 이것을 "오버라이딩(overriding)" 이라 부른단
+  // => 이것을 "오버라이딩(overriding)"이라 부른다.
+  //
   public boolean equals(Object obj) {
     if (obj == null) {
       return false;
@@ -47,10 +43,10 @@ public class Member {
 
     // 위 조건에서 this가 가리키는 인스턴스의 클래스와
     // 파라미터 obj가 가리키는 인스턴스의 클래스가
-    // 깉디고 결론이 났기 떄믄에 다음과 같이
-    // obj를 Member타입으로 형변환한다.
-
+    // 같다고 결론이 났기 때문에 다음과 같이
+    // obj를 Member 타입으로 형변환한다.
     Member m = (Member) obj;
+
     if (this.getNo() != m.getNo()) {
       return false;
     }
@@ -63,12 +59,11 @@ public class Member {
     // return false;
     // }
     //
-    // if (this.getPassword() != null &&
-    // !this.getPassword().equals(m.getPassword())) {
+    // if (this.getPassword() != null && !this.getPassword().equals(m.getPassword())) {
     // return false;
     // }
     //
-    // if (this.getGender() != (m.getGender())) {
+    // if (this.getGender() != m.getGender()) {
     // return false;
     // }
 
