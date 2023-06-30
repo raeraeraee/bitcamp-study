cf031b3f535b4aa2247f64ab81fb2db1683649ee
raeraeraee <rlatjsfo674@naver.com>
2023-06-30 17:45:57 +0900

    자바 Collection API 사용하기

---
+++ project/app-26/src/main/java/bitcamp/myapp/handler/MemberUpdateListener.java
@@ -0,0 +1,28 @@
+package bitcamp.myapp.handler;
+
+import java.util.List;
+import bitcamp.myapp.vo.Member;
+import bitcamp.util.BreadcrumbPrompt;
+
+public class MemberUpdateListener extends AbstractMemberListener {
+
+  public MemberUpdateListener(List<Member> list) {
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    int memberNo = prompt.inputInt("번호? ");
+
+    Member m = this.findBy(memberNo);
+    if (m == null) {
+      System.out.println("해당 번호의 회원이 없습니다!");
+      return;
+    }
+
+    m.setName(prompt.inputString("이름(%s)? ", m.getName()));
+    m.setGit_push(inputGit_push(m.getGit_push(), prompt));
+    m.setProgrammers(inputProgrammers(m.getProgrammers(), prompt));
+  }
+
+}
