809ae2f9e9d517bb15452c37672c451c498d30bc
raeraeraee <rlatjsfo674@naver.com>
2023-07-02 19:05:47 +0900

    상속을 이용하여 primitive type과 String 출력 기능을 추가하기

---
+++ project/app-28/src/main/java/bitcamp/myapp/handler/MemberListListener.java
@@ -0,0 +1,31 @@
+package bitcamp.myapp.handler;
+
+import java.util.Iterator;
+import java.util.List;
+import bitcamp.myapp.vo.Member;
+import bitcamp.util.BreadcrumbPrompt;
+
+public class MemberListListener extends AbstractMemberListener {
+
+  public MemberListListener(List<Member> list) {
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    System.out.println("---------------------------------------");
+    System.out.println("번호, 이름, 깃 잔디, 프로그래머스");
+    System.out.println("---------------------------------------");
+
+    // 목록에서 데이터를 대신 꺼내주는 객체를 얻는다.
+    Iterator<Member> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      Member m = iterator.next();
+      System.out.printf("%d, %s, %s, %s\n",
+          m.getNo(), m.getName(),
+          toGit_pushString(m.getGit_push()),
+          toProgrammerString(m.getProgrammers()));
+    }
+  }
+
+}
