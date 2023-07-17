82d30976344091fcb30c195476b31305a2469cbb
rae <rae@localhost>
2023-07-17 17:33:57 +0900

    스레드 재사용하기 : 자바에서 제공하는 스레드풀(thread pool) 사용

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -8,134 +8,161 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import bitcamp.myapp.dao.BoardListDao;
 import bitcamp.myapp.dao.MemberListDao;
 import bitcamp.net.RequestEntity;
 import bitcamp.net.ResponseEntity;
-import bitcamp.util.ManagedThread;
-import bitcamp.util.ThreadPool;
 
 public class ServerApp {
 
-  int port;
-  ServerSocket serverSocket;
+	int port;
+	ServerSocket serverSocket;
 
-  HashMap<String, Object> daoMap = new HashMap<>();
+	HashMap<String,Object> daoMap = new HashMap<>();
 
-  // 스레드를 리턴해 줄 스레드풀 준비
-  ThreadPool threadPool = new ThreadPool();
+	// 자바 스레드풀 준비
+	ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
-  public ServerApp(int port) throws Exception {
-    this.port = port;
+	public ServerApp(int port) throws Exception {
+		this.port = port;
 
-    daoMap.put("member", new MemberListDao("member.json"));
-    daoMap.put("board", new BoardListDao("board.json"));
-    daoMap.put("reading", new BoardListDao("reading.json"));
-  }
+		daoMap.put("member", new MemberListDao("member.json"));
+		daoMap.put("board", new BoardListDao("board.json"));
+		daoMap.put("reading", new BoardListDao("reading.json"));
+	}
 
-  public void close() throws Exception {
-    serverSocket.close();
-  }
+	public void close() throws Exception {
+		serverSocket.close();
+	}
 
-  public static void main(String[] args) throws Exception {
-    if (args.length < 1) {
-      System.out.println("실행 예) java ... bitcamp.myapp.ServerApp 포트번호");
-      return;
-    }
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			System.out.println("실행 예) java ... bitcamp.myapp.ServerApp 포트번호");
+			return;
+		}
 
-    ServerApp app = new ServerApp(Integer.parseInt(args[0]));
-    app.execute();
-    app.close();
-  }
+		ServerApp app = new ServerApp(Integer.parseInt(args[0]));
+		app.execute();
+		app.close();
+	}
 
-  public void execute() throws Exception {
-    System.out.println("[MyList 서버 애플리케이션]");
+	public void execute() throws Exception {
+		System.out.println("[MyList 서버 애플리케이션]");
 
-    this.serverSocket = new ServerSocket(port);
-    System.out.println("서버 실행 중...");
+		this.serverSocket = new ServerSocket(port);
+		System.out.println("서버 실행 중...");
 
-    while (true) {
-      Socket socket = serverSocket.accept();
-      ManagedThread t = threadPool.getResource();
-      t.setJob(() -> processRequest(socket));
-    }
-  }
+		while (true) {
+			Socket socket = serverSocket.accept();
 
-  public static Method findMethod(Object obj, String methodName) {
-    Method[] methods = obj.getClass().getDeclaredMethods();
-    for (int i = 0; i < methods.length; i++) {
-      if (methods[i].getName().equals(methodName)) {
-        return methods[i];
-      }
-    }
-    return null;
-  }
+			threadPool.execute(() -> processRequest(socket));
 
-  public static Object call(Object obj, Method method, RequestEntity request) throws Exception {
-    Parameter[] params = method.getParameters();
-    if (params.length > 0) {
-      return method.invoke(obj, request.getObject(params[0].getType()));
-    } else {
-      return method.invoke(obj);
-    }
-  }
+			// 컴파일러는 위의 문장을 다음 문장으로 변환한다.
+			//      class $1 implements Runnable {
+			//        ServerApp this$0;
+			//        Socket socket;
+			//
+			//        public $1(ServerApp arg0, Socket arg1) {
+			//          this$0 = arg0;
+			//          socket = arg1;
+			//        }
+			//
+			//        public void run() {
+			//          this$0.processRequest(socket);
+			//        }
+			//      }
+			//      $1 obj = new $1(this, socket);
+			//      threadPool.execute(obj);
+		}
+	}
 
-  public void processRequest(Socket socket) {
-    try (Socket s = socket;
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+	public static Method findMethod(Object obj, String methodName) {
+		Method[] methods = obj.getClass().getDeclaredMethods();
+		for (int i = 0; i < methods.length; i++) {
+			if (methods[i].getName().equals(methodName)) {
+				return methods[i];
+			}
+		}
+		return null;
+	}
 
-      InetSocketAddress socketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-      System.out.printf("%s:%s 클라이언트가 접속했음!\n", socketAddress.getHostString(),
-          socketAddress.getPort());
+	public static Object call(Object obj, Method method, RequestEntity request) throws Exception {
+		Parameter[] params = method.getParameters();
+		if (params.length > 0) {
+			return method.invoke(obj, request.getObject(params[0].getType()));
+		} else {
+			return method.invoke(obj);
+		}
+	}
 
-      // 스레드풀이 새 스레드를 만드는 것을 테스트하기 위함.
-      // => 스레드풀에 스레드가 없을 때 새 스레드를 만들 것이다.
-      Thread.sleep(10000);
+	public void processRequest(Socket socket) {
+		try (Socket s = socket;
+				DataInputStream in = new DataInputStream(socket.getInputStream());
+				DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
 
-      // 클라이언트 요청을 반복해서 처리하지 않는다.
-      // => 접속 -> 요청 -> 실행 -> 응답 -> 연결 끊기
-      RequestEntity request = RequestEntity.fromJson(in.readUTF());
+			InetSocketAddress socketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+			System.out.printf("[%s] %s:%s 클라이언트가 접속했음!\n",
+					Thread.currentThread().getName(),
+					socketAddress.getHostString(),
+					socketAddress.getPort());
 
-      String command = request.getCommand();
-      System.out.println(command);
+			// 스레드풀이 새 스레드를 만드는 것을 테스트하기 위함.
+			// => 스레드풀에 스레드가 없을 때 새 스레드를 만들 것이다.
+			//      Thread.sleep(10000);
 
-      String[] values = command.split("/");
-      String dataName = values[0];
-      String methodName = values[1];
+			// 클라이언트 요청을 반복해서 처리하지 않는다.
+			// => 접속 -> 요청 -> 실행 -> 응답 -> 연결 끊기
+			RequestEntity request = RequestEntity.fromJson(in.readUTF());
 
-      Object dao = daoMap.get(dataName);
-      if (dao == null) {
-        out.writeUTF(
-            new ResponseEntity().status(ResponseEntity.ERROR).result("데이터를 찾을 수 없습니다.").toJson());
-        return;
-      }
+			String command = request.getCommand();
+			System.out.println(command);
 
-      Method method = findMethod(dao, methodName);
-      if (method == null) {
-        out.writeUTF(
-            new ResponseEntity().status(ResponseEntity.ERROR).result("메서드를 찾을 수 없습니다.").toJson());
-        return;
-      }
+			String[] values = command.split("/");
+			String dataName = values[0];
+			String methodName = values[1];
 
-      try {
-        Object result = call(dao, method, request);
+			Object dao = daoMap.get(dataName);
+			if (dao == null) {
+				out.writeUTF(new ResponseEntity()
+						.status(ResponseEntity.ERROR)
+						.result("데이터를 찾을 수 없습니다.")
+						.toJson());
+				return;
+			}
 
-        ResponseEntity response = new ResponseEntity();
-        response.status(ResponseEntity.SUCCESS);
-        response.result(result);
-        out.writeUTF(response.toJson());
+			Method method = findMethod(dao, methodName);
+			if (method == null) {
+				out.writeUTF(new ResponseEntity()
+						.status(ResponseEntity.ERROR)
+						.result("메서드를 찾을 수 없습니다.")
+						.toJson());
+				return;
+			}
 
-      } catch (Exception e) {
-        ResponseEntity response = new ResponseEntity();
-        response.status(ResponseEntity.ERROR);
-        response.result(e.getMessage());
-        out.writeUTF(response.toJson());
-      }
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-    }
-  }
+			try {
+				Object result = call(dao, method, request);
+
+				ResponseEntity response = new ResponseEntity();
+				response.status(ResponseEntity.SUCCESS);
+				response.result(result);
+				out.writeUTF(response.toJson());
+
+			} catch (Exception e) {
+				ResponseEntity response = new ResponseEntity();
+				response.status(ResponseEntity.ERROR);
+				response.result(e.getMessage());
+				out.writeUTF(response.toJson());
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
+
+
+
 
 
