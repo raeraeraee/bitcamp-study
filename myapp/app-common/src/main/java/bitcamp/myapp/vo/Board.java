0c84c1aff180b398b09f9f033064b023176b2481
raeraeraee <rlatjsfo674@naver.com>
2023-07-21 18:02:11 +0900

    DBMS 도입하기

--- myapp/app-common/src/main/java/bitcamp/myapp/vo/Board.java
+++ myapp/app-common/src/main/java/bitcamp/myapp/vo/Board.java
@@ -2,129 +2,84 @@ package bitcamp.myapp.vo;
 
 import java.io.Serializable;
 
-public class Board implements Serializable, CsvObject, AutoIncrement {
-  private static final long serialVersionUID = 1L;
+public class Board implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-  public static int boardNo = 1;
+	private int no;
+	private String title;
+	private String content;
+	private String writer;
+	private String password;
+	private int viewCount;
+	private long createdDate;
 
-  private int no;
-  private String title;
-  private String content;
-  private String writer;
-  private String password;
-  private int viewCount;
-  private long createdDate;
+	public Board() {}
 
-  public Board() {}
+	public Board(int no) {
+		this.no = no;
+	}
 
-  public Board(int no) {
-    this.no = no;
-  }
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
 
-  public static Board fromCsv(String csv) {
-    String[] values = csv.split(",");
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
 
-    Board board = new Board(Integer.parseInt(values[0]));
-    board.setTitle(values[1]);
-    board.setContent(values[2]);
-    board.setWriter(values[3]);
-    board.setPassword(values[4]);
-    board.setViewCount(Integer.parseInt(values[5]));
-    board.setCreatedDate(Long.parseLong(values[6]));
+		Board b = (Board) obj;
 
-    if (Board.boardNo <= board.getNo()) {
-      Board.boardNo = board.getNo() + 1;
-    }
+		if (this.getNo() != b.getNo()) {
+			return false;
+		}
 
-    return board;
-  }
+		return true;
+	}
 
-  @Override
-  public void updateKey() {
-    if (Board.boardNo <= this.no) {
-      Board.boardNo = this.no + 1;
-    }
-  }
-
-  @Override
-  public String toCsvString() {
-    return String.format("%d,%s,%s,%s,%s,%d,%d", this.getNo(), this.getTitle(), this.getContent(),
-        this.getWriter(), this.getPassword(), this.getViewCount(), this.getCreatedDate());
-  }
-
-  public boolean equals(Object obj) {
-    if (obj == null) {
-      return false;
-    }
-
-    if (this.getClass() != obj.getClass()) {
-      return false;
-    }
-
-    Board b = (Board) obj;
-
-    if (this.getNo() != b.getNo()) {
-      return false;
-    }
-
-    return true;
-  }
-
-  public int getNo() {
-    return no;
-  }
-
-  public void setNo(int no) {
-    this.no = no;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
-
-  public String getWriter() {
-    return writer;
-  }
-
-  public void setWriter(String writer) {
-    this.writer = writer;
-  }
-
-  public int getViewCount() {
-    return viewCount;
-  }
-
-  public void setViewCount(int viewCount) {
-    this.viewCount = viewCount;
-  }
-
-  public long getCreatedDate() {
-    return createdDate;
-  }
-
-  public void setCreatedDate(long createdDate) {
-    this.createdDate = createdDate;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getViewCount() {
+		return viewCount;
+	}
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
+	public long getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(long createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 
 }
