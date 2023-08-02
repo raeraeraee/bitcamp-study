850119f9c667864347487896c3966a3b7b8e08ba
raeraeraee <rlatjsfo674@naver.com>
2023-08-02 17:51:06 +0900

    웹 애플리케이션 서버 구조로 전환하기 - 웹 기술 도입

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -1,152 +1,75 @@
 package bitcamp.myapp;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
-import bitcamp.myapp.dao.BoardDao;
-import bitcamp.myapp.dao.MemberDao;
-import bitcamp.myapp.dao.MySQLBoardDao;
-import bitcamp.myapp.dao.MySQLMemberDao;
-import bitcamp.myapp.handler.BoardAddListener;
-import bitcamp.myapp.handler.BoardDeleteListener;
-import bitcamp.myapp.handler.BoardDetailListener;
-import bitcamp.myapp.handler.BoardListListener;
-import bitcamp.myapp.handler.BoardUpdateListener;
-import bitcamp.myapp.handler.LoginListener;
-import bitcamp.myapp.handler.MemberAddListener;
-import bitcamp.myapp.handler.MemberDeleteListener;
-import bitcamp.myapp.handler.MemberDetailListener;
-import bitcamp.myapp.handler.MemberListListener;
-import bitcamp.myapp.handler.MemberUpdateListener;
-import bitcamp.net.NetProtocol;
-import bitcamp.util.BreadcrumbPrompt;
-import bitcamp.util.Menu;
-import bitcamp.util.MenuGroup;
+import bitcamp.myapp.config.AppConfig;
+import bitcamp.util.ApplicationContext;
+import bitcamp.util.DispatcherServlet;
+import bitcamp.util.HttpServletRequest;
+import bitcamp.util.HttpServletResponse;
 import bitcamp.util.SqlSessionFactoryProxy;
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.NettyOutbound;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.server.HttpServerRequest;
+import reactor.netty.http.server.HttpServerResponse;
 
 public class ServerApp {
 
-	// 자바 스레드풀 준비
-	ExecutorService threadPool = Executors.newFixedThreadPool(2);
+  ApplicationContext iocContainer;
+  DispatcherServlet dispatcherServlet;
 
-	SqlSessionFactory sqlSessionFactory;
+  int port;
 
-	MemberDao memberDao;
-	BoardDao boardDao;
+  public ServerApp(int port) throws Exception {
+    this.port = port;
+    iocContainer = new ApplicationContext(AppConfig.class);
+    dispatcherServlet = new DispatcherServlet(iocContainer);
+  }
 
-	MenuGroup mainMenu = new MenuGroup("메인");
+  public void close() throws Exception {
 
-	int port;
+  }
 
-	public ServerApp(int port) throws Exception {
+  public static void main(String[] args) throws Exception {
+    ServerApp app = new ServerApp(8888);
+    app.execute();
+    app.close();
+  }
 
-		this.port = port;
+  public void execute() throws Exception {
+    DisposableServer server = HttpServer
+        .create()
+        .port(8888)
+        .handle((request, response) -> processRequest(request, response))
+        .bindNow();
+    System.out.println("서버 실행됨!");
 
-		// 1) mybatis 설정 파일을 읽어들일 도구를 준비한다.
-		InputStream mybatisConfigIn = Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml");
+    server.onDispose().block();
+    System.out.println("서버 종료됨!");
+  }
 
-		// 2) SqlSessionFactory를 만들어줄 빌더 객체 준비
-		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+  private NettyOutbound processRequest(HttpServerRequest request, HttpServerResponse response) {
+    HttpServletRequest request2 = new HttpServletRequest(request);
+    HttpServletResponse response2 = new HttpServletResponse(response);
 
-		// 3) 빌더 객체를 통해 SqlSessionFactory를 생성
-		sqlSessionFactory = new SqlSessionFactoryProxy(builder.build(mybatisConfigIn));
+    try {
+      dispatcherServlet.service(request2, response2);
 
-		this.memberDao = new MySQLMemberDao(sqlSessionFactory);
-		this.boardDao = new MySQLBoardDao(sqlSessionFactory);
+      // HTTP 응답 프로토콜의 헤더 설정
+      response.addHeader("Content-Type", response2.getContentType());
 
-		prepareMenu();
-	}
+      // 서블릿이 출력한 문자열을 버퍼에서 꺼내 HTTP 프로토콜에 맞춰 응답한다.
+      return response.sendString(Mono.just(response2.getContent()));
 
-	public void close() throws Exception {
+    } catch (Exception e) {
+      e.printStackTrace();
+      return response.sendString(Mono.just(response2.getContent()));
 
-	}
-
-	public static void main(String[] args) throws Exception {
-		ServerApp app = new ServerApp(8888);
-		app.execute();
-		app.close();
-	}
-
-	public void execute() {
-		try (ServerSocket serverSocket = new ServerSocket(this.port)) {
-			System.out.println("서버 실행 중...");
-
-			while (true) {
-				Socket socket = serverSocket.accept();
-				threadPool.execute(() -> processRequest(socket));
-			}
-		} catch (Exception e) {
-			System.out.println("서버 실행 오류!");
-			e.printStackTrace();
-		}
-	}
-
-	private void processRequest(Socket socket) {
-		try (Socket s = socket;
-				DataInputStream in = new DataInputStream(socket.getInputStream());
-				DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
-
-			BreadcrumbPrompt prompt = new BreadcrumbPrompt(in, out);
-
-			InetSocketAddress clientAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-			System.out.printf("%s 클라이언트 접속함!\n", clientAddress.getHostString());
-
-			out.writeUTF("[나의 목록 관리 시스템]\n"
-					+ "-----------------------------------------");
-
-			new LoginListener(memberDao).service(prompt);
-
-			mainMenu.execute(prompt);
-			out.writeUTF(NetProtocol.NET_END);
-
-		} catch (Exception e) {
-			System.out.println("클라이언트 통신 오류!");
-			e.printStackTrace();
-
-		} finally {
-			((SqlSessionFactoryProxy) sqlSessionFactory).clean();
-		}
-	}
-
-	private void prepareMenu() {
-		MenuGroup memberMenu = new MenuGroup("회원");
-		memberMenu.add(new Menu("등록", new MemberAddListener(memberDao, sqlSessionFactory)));
-		memberMenu.add(new Menu("목록", new MemberListListener(memberDao)));
-		memberMenu.add(new Menu("조회", new MemberDetailListener(memberDao)));
-		memberMenu.add(new Menu("변경", new MemberUpdateListener(memberDao, sqlSessionFactory)));
-		memberMenu.add(new Menu("삭제", new MemberDeleteListener(memberDao, sqlSessionFactory)));
-		mainMenu.add(memberMenu);
-
-		MenuGroup boardMenu = new MenuGroup("게시글");
-		boardMenu.add(new Menu("등록", new BoardAddListener(1, boardDao, sqlSessionFactory)));
-		boardMenu.add(new Menu("목록", new BoardListListener(1, boardDao)));
-		boardMenu.add(new Menu("조회", new BoardDetailListener(1, boardDao, sqlSessionFactory)));
-		boardMenu.add(new Menu("변경", new BoardUpdateListener(1, boardDao, sqlSessionFactory)));
-		boardMenu.add(new Menu("삭제", new BoardDeleteListener(1, boardDao, sqlSessionFactory)));
-		mainMenu.add(boardMenu);
-
-		MenuGroup readingMenu = new MenuGroup("독서록");
-		readingMenu.add(new Menu("등록", new BoardAddListener(2, boardDao, sqlSessionFactory)));
-		readingMenu.add(new Menu("목록", new BoardListListener(2, boardDao)));
-		readingMenu.add(new Menu("조회", new BoardDetailListener(2, boardDao, sqlSessionFactory)));
-		readingMenu.add(new Menu("변경", new BoardUpdateListener(2, boardDao, sqlSessionFactory)));
-		readingMenu.add(new Menu("삭제", new BoardDeleteListener(2, boardDao, sqlSessionFactory)));
-		mainMenu.add(readingMenu);
-
-		//    Menu helloMenu = new Menu("안녕!");
-		//    helloMenu.addActionListener(new HeaderListener());
-		//    helloMenu.addActionListener(new HelloListener());
-		//    helloMenu.addActionListener(new FooterListener());
-		//    mainMenu.add(helloMenu);
-	}
+    } finally {
+      SqlSessionFactoryProxy sqlSessionFactoryProxy =
+          (SqlSessionFactoryProxy) iocContainer.getBean(SqlSessionFactory.class);
+      sqlSessionFactoryProxy.clean();
+    }
+  }
 }
--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddServlet.java
@@ -2,32 +2,35 @@ package bitcamp.myapp.handler;
 
 import java.io.PrintWriter;
 import org.apache.ibatis.session.SqlSessionFactory;
-import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
 import bitcamp.util.Component;
 import bitcamp.util.HttpServletRequest;
 import bitcamp.util.HttpServletResponse;
 import bitcamp.util.Servlet;
 
-@Component("/member/add")
-public class MemberAddListener implements Servlet {
+@Component("/board/add")
+public class BoardAddServlet implements Servlet {
 
-  MemberDao memberDao;
+  BoardDao boardDao;
   SqlSessionFactory sqlSessionFactory;
 
-  public MemberAddListener(MemberDao memberDao, SqlSessionFactory sqlSessionFactory) {
-    this.memberDao = memberDao;
+  public BoardAddServlet(BoardDao boardDao, SqlSessionFactory sqlSessionFactory) {
+    this.boardDao = boardDao;
     this.sqlSessionFactory = sqlSessionFactory;
   }
 
   @Override
   public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-    Member m = new Member();
-    m.setName(request.getParameter("name"));
-    m.setEmail(request.getParameter("email"));
-    m.setPassword(request.getParameter("password"));
-    m.setGender(request.getParameter("gender").charAt(0));
+    int category = Integer.parseInt(request.getParameter("category"));
+
+    Board board = new Board();
+    board.setTitle(request.getParameter("title"));
+    board.setContent(request.getParameter("content"));
+    board.setWriter((Member) request.getAttribute("loginUser"));
+    board.setCategory(category);
 
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
@@ -35,14 +38,13 @@ public class MemberAddListener implements Servlet {
     out.println("<html>");
     out.println("<head>");
     out.println("<meta charset='UTF-8'>");
-    out.println("<meta http-equiv='refresh' content='1;url=/member/list'>");
-    out.println("<title>회원</title>");
+    out.printf("<meta http-equiv='refresh' content='1;url=/board/list?category=%d'>\n", category);
+    out.println("<title>게시글</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>회원 등록</h1>");
-
+    out.println("<h1>게시글 등록</h1>");
     try {
-      memberDao.insert(m);
+      boardDao.insert(board);
       sqlSessionFactory.openSession(false).commit();
       out.println("<p>등록 성공입니다!</p>");
 
@@ -51,8 +53,18 @@ public class MemberAddListener implements Servlet {
       out.println("<p>등록 실패입니다!</p>");
       e.printStackTrace();
     }
-
     out.println("</body>");
     out.println("</html>");
   }
 }
+
+
+
+
+
+
+
+
+
+
+
