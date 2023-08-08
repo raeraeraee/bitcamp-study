8fb148bd62ba42d4a0696adc20a8f1f26d4db428
raeraeraee <rlatjsfo674@naver.com>
2023-08-08 16:04:10 +0900

    웹 애플리케이션 자바 표준 기술 JavaEE 도입(계속)

--- servlet-app/app/src/main/java/eomcs/servlet/ex01/Servlet02.java
+++ servlet-app/app/src/main/java/eomcs/servlet/ex01/Servlet02.java
@@ -1,7 +1,6 @@
 package eomcs.servlet.ex01;
 
 import java.io.IOException;
-
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -14,9 +13,9 @@ import javax.servlet.annotation.WebServlet;
 //=> 따라서 이 클래스를 상속 받는 서브 클래스는 service() 만 구현하면 된다.
 //
 //@WebServlet 애노테이션
-//=> web.xml 에 서블릿 정보를 설정하는 대신에 
+//=> web.xml 에 서블릿 정보를 설정하는 대신에
 // 이 애노테이션을 사용하여 서블릿을 설정할 수 있다.
-//=> 이 애노테이션을 활성화시키려면 
+//=> 이 애노테이션을 활성화시키려면
 // web.xml의 <web-app> 태그에 다음 속성을 추가해야 한다.
 //   metadata-complete="false"
 // 속성의 값은 false 여야 한다.
@@ -28,19 +27,17 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet("/ex01/s2")
 public class Servlet02 extends GenericServlet {
 
-	// GenericServlet 추상 클래스는 java.io.Serialize 인터페이스를 구현하였다.
-	// => serialVersionUID 변수 값을 설정해야 한다.
-	private static final long serialVersionUID = 1L;
+  // GenericServlet 추상 클래스는 java.io.Serialize 인터페이스를 구현하였다.
+  // => serialVersionUID 변수 값을 설정해야 한다.
+  private static final long serialVersionUID = 1L;
 
-	public Servlet02() {
-		System.out.println("Servlet02()");
-	}
+  public Servlet02() {
+    System.out.println("Servlet02()");
+  }
 
-	@Override
-	public void service(ServletRequest req, ServletResponse res)
-			throws ServletException, IOException {
-		System.out.println("Servlet02.service(ServletRequest,ServletResponse)");
-	}
+  @Override
+  public void service(ServletRequest req, ServletResponse res)
+      throws ServletException, IOException {
+    System.out.println("Servlet02.service(ServletRequest,ServletResponse)");
+  }
 }
-
-
--- servlet-app/app/src/main/java/eomcs/servlet/ex01/Servlet03.java
+++ servlet-app/app/src/main/java/eomcs/servlet/ex01/Servlet03.java
@@ -1,7 +1,6 @@
 package eomcs.servlet.ex01;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +10,16 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/ex01/s3")
 public class Servlet03 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		System.out.println("Servlet03.service() 호출됨!");
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    System.out.println("Servlet03.service() 호출됨!");
 
-		HttpSession session =  req.getSession();
-		resp.sendRedirect("/ex01/s2");
-	}
+    HttpSession session =  req.getSession();
+    resp.sendRedirect("/ex01/s2");
+  }
 }
 
 
